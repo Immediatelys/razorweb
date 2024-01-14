f0f0f6d38a033618d2e456ae576249d317370250
Thắng Lại <laiducthanghb.2003@gmail.com>
2024-01-14 22:26:24 +0700

    tich hop EF

---
+++ Models/Article.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RazorWeb.Models
+{
+    public class Article
+    {
+        [Key]
+        public int  Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        [Column(TypeName = "nvarchar")]
+        public string Title { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime Created { get; set; }
+
+        public string Content { get; set; }
+    }
+}
