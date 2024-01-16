01e4ecdd5dd2c775562054380a22d746d92c2725
Thắng Lại <laiducthanghb.2003@gmail.com>
2024-01-16 10:30:27 +0700

    add identity

--- Migrations/20240115151017_AddDatabase.Designer.cs
+++ Migrations/20240115151017_AddDatabase.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RazorWeb.Models;
 
@@ -11,9 +12,10 @@ using RazorWeb.Models;
 namespace RazorWeb.Migrations
 {
     [DbContext(typeof(MyBlogContext))]
-    partial class MyBlogContextModelSnapshot : ModelSnapshot
+    [Migration("20240115151017_AddDatabase")]
+    partial class AddDatabase
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
